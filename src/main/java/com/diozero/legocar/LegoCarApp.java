e9f586ad179d0579fa27159702a3552d0de2b45d
Matthew Lewis <matthew.lewis@mulesoft.com>
2021-07-29 17:49:05 +0100

    Joystick API fixes

--- src/main/java/com/diozero/legocar/LegoCarApp.java
+++ src/main/java/com/diozero/legocar/LegoCarApp.java
@@ -4,27 +4,25 @@ import org.tinylog.Logger;
 
 import com.diozero.api.AnalogOutputDevice;
 import com.diozero.api.ServoDevice;
+import com.diozero.api.ServoTrim;
 import com.diozero.devices.LED;
 import com.diozero.devices.PiconZero;
+import com.diozero.devices.motor.AnalogOutputMotor;
+import com.diozero.devices.motor.MotorInterface;
 import com.diozero.sdl.joystick.GameController;
+import com.diozero.sdl.joystick.Joystick;
+import com.diozero.sdl.joystick.JoystickAxisMotionListener;
+import com.diozero.sdl.joystick.JoystickButtonListener;
+import com.diozero.sdl.joystick.JoystickDeviceListener;
 import com.diozero.sdl.joystick.JoystickEvent;
-import com.diozero.sdl.joystick.JoystickEvent.AxisMotionEvent;
-import com.diozero.sdl.joystick.JoystickEvent.ButtonEvent;
-import com.diozero.sdl.joystick.JoystickEvent.DeviceEvent;
-import com.diozero.sdl.joystick.JoystickEventListener;
 import com.diozero.sdl.joystick.JoystickInfo;
 import com.diozero.sdl.joystick.JoystickNative;
 import com.diozero.util.Diozero;
-import com.diozero.util.RangeUtil;
+import com.diozero.util.Hex;
 import com.diozero.util.SleepUtil;
 
-public class LegoCarApp implements AutoCloseable, JoystickEventListener {
-	// Picon Zero connections
-	private static final int STEERING_GPIO = 0;
-	private static final int LED1_GPIO = 3;
-	private static final int LED2_GPIO = 4;
-	private static final int SERVO_MAX_ANGLE_FROM_CENTRE = 80;
-
+public class LegoCarApp
+		implements AutoCloseable, JoystickDeviceListener, JoystickButtonListener, JoystickAxisMotionListener {
 	private static final boolean TEST_SERVO_ON_START = false;
 	private static final boolean TEST_LEDS_ON_START = false;
 
@@ -37,7 +35,7 @@ public class LegoCarApp implements AutoCloseable, JoystickEventListener {
 		}
 		if (JoystickNative.getNumJoysticks() == 0) {
 			Logger.error("No joysticks");
-			System.exit(1);
+			return;
 		}
 
 		try (LegoCarApp app = new LegoCarApp()) {
@@ -49,6 +47,7 @@ public class LegoCarApp implements AutoCloseable, JoystickEventListener {
 				app.testLEDs();
 			}
 
+			Joystick.addDeviceListener(app);
 			JoystickNative.processEvents();
 		} catch (Exception e) {
 			Logger.error(e, "Error: {}", e);
@@ -57,100 +56,106 @@ public class LegoCarApp implements AutoCloseable, JoystickEventListener {
 		}
 	}
 
-	public static int calcServoAngle(float value) {
-		return (int) (RangeUtil.constrain(value, -1, 1) * SERVO_MAX_ANGLE_FROM_CENTRE) + PiconZero.SERVO_CENTRE;
-	}
+	// Picon Zero connections
+	private static final int STEERING_GPIO = 0;
+	private static final int LED1_GPIO = 3;
+	private static final int LED2_GPIO = 4;
+	// Don't need the full servo range (typical range is 2 * 900 us)
+	private static final ServoTrim SERVO_TRIM = new ServoTrim(ServoTrim.DEFAULT_MID_US, ServoTrim.DEFAULT_90_DELTA_US,
+			2 * 700);
 
+	private GameController gameController;
 	private PiconZero pz;
 	private ServoDevice steering;
 	private LED light1;
 	private LED light2;
-	private AnalogOutputDevice motor;
-	private GameController joystick;
+	private MotorInterface motor;
 
 	private LegoCarApp() {
-		joystick = (GameController) JoystickNative.getJoystickOrGameController(0);
-		joystick.setListener(this);
+		gameController = (GameController) JoystickNative.getJoystickOrGameController(0);
+		Logger.info("gameController type: {}, GUID: {}", gameController.getGameControllerType(),
+				Hex.encodeHexString(gameController.getGuid()));
+
+		gameController.setButtonListener(this);
+		gameController.setAxisMotionListener(this);
 
 		pz = new PiconZero();
 		// Configure the outputs
-		steering = ServoDevice.newBuilder(STEERING_GPIO).setDeviceFactory(pz).build();
+		steering = ServoDevice.newBuilder(STEERING_GPIO).setTrim(SERVO_TRIM).setDeviceFactory(pz).build();
 		light1 = new LED(pz, LED1_GPIO);
 		light2 = new LED(pz, LED2_GPIO);
-		motor = AnalogOutputDevice.Builder.builder(PiconZero.PiconZeroBoardPinInfo.MOTOR2_GPIO).setDeviceFactory(pz)
-				.build();
+		motor = new AnalogOutputMotor(AnalogOutputDevice.Builder.builder(PiconZero.PiconZeroBoardPinInfo.MOTOR2_GPIO)
+				.setDeviceFactory(pz).build());
 	}
 
 	@Override
-	public void accept(JoystickEvent event) {
-		Logger.info("Joystick event: {}", event);
-
-		if (event.getCategory() == JoystickEvent.Category.DEVICE) {
-			DeviceEvent d_event = (DeviceEvent) event;
-			switch (d_event.getType()) {
-			case GAME_CONTROLLER_ADDED:
-				if (joystick == null) {
-					joystick = (GameController) JoystickNative.getJoystickOrGameController(event.getJoystickId());
-				}
-				break;
-			case GAME_CONTROLLER_REMOVED:
-				if (joystick != null && joystick.getId() == event.getJoystickId()) {
-					joystick.close();
-					joystick = null;
-				}
-				break;
-			default:
-				Logger.info("Unhandled device event: {}", event);
-				// Ignore
+	public void accept(JoystickEvent.DeviceEvent event) {
+		switch (event.getType()) {
+		case GAME_CONTROLLER_ADDED:
+			if (gameController == null) {
+				gameController = (GameController) JoystickNative.getJoystickOrGameController(event.getJoystickId());
 			}
-		} else if (joystick != null) {
-			switch (event.getCategory()) {
-			case BUTTON_PRESS:
-				ButtonEvent b_event = (ButtonEvent) event;
-				GameController.Button button = joystick.getButtonMapping(b_event.getButton());
-				if (button == null) {
-					Logger.error("Button was null for {}", b_event);
-				} else {
-					Logger.debug("Got button {}", button);
-					switch (button) {
-					case GUIDE:
-						JoystickNative.stopEventLoop();
-						break;
-					case A:
-						light1.setOn(b_event.isPressed());
-						break;
-					case B:
-						light2.setOn(b_event.isPressed());
-						break;
-					default:
-						Logger.info("Unhandled button event {}", b_event);
-					}
-				}
-				break;
-			case AXIS_MOTION:
-				AxisMotionEvent a_event = (AxisMotionEvent) event;
-				GameController.Axis axis = joystick.getAxisMapping(a_event.getAxis());
-				if (axis == null) {
-					Logger.error("Axis was null for {}", a_event);
-				} else {
-					Logger.debug("Got axis {}", axis);
-					switch (axis) {
-					case LEFTX:
-						steering.setAngle(calcServoAngle(a_event.getValue()));
-						break;
-					case RIGHTX:
-						// Invert the axis value
-						motor.setValue(-a_event.getValue());
-						break;
-					default:
-						Logger.info("Unhandled axis motion event: {}", a_event);
-					}
-				}
-				break;
-			default:
-				// Ignore
+			break;
+		case GAME_CONTROLLER_REMOVED:
+			if (gameController != null && gameController.getId() == event.getJoystickId()) {
+				gameController.close();
+				gameController = null;
 			}
+			break;
+		default:
+			Logger.info("Unhandled device event: {}", event);
+			// Ignore
 		}
+	}
+
+	@Override
+	public void accept(JoystickEvent.ButtonEvent event) {
+		GameController.Button button = event.getButton(gameController);
+		Logger.debug("Got button {}", button);
+
+		switch (button) {
+		case GUIDE:
+			JoystickNative.stopEventLoop();
+			break;
+		case A:
+			light1.setOn(event.isPressed());
+			break;
+		case B:
+			light2.setOn(event.isPressed());
+			break;
+		default:
+			Logger.info("Unhandled button event {}", event);
+		}
+	}
+
+	@Override
+	public void accept(JoystickEvent.AxisMotionEvent event) {
+		GameController.Axis axis = event.getAxis(gameController);
+		Logger.debug("Got axis {}", axis);
+
+		switch (axis) {
+		case LEFTX:
+			steering.setValue(event.getValue());
+			break;
+		case RIGHTY:
+			// Invert the axis value
+			motor.setValue(-event.getValue());
+			break;
+		default:
+			Logger.info("Unhandled axis motion event: {}", event);
+		}
+	}
+
+	@Override
+	public void close() {
+		if (gameController != null) {
+			gameController.close();
+		}
+		motor.close();
+		steering.close();
+		light1.close();
+		light2.close();
+		pz.close();
 	}
 
 	private void testServo() {
@@ -159,13 +164,13 @@ public class LegoCarApp implements AutoCloseable, JoystickEventListener {
 		pz.setOutputValue(STEERING_GPIO, PiconZero.SERVO_CENTRE);
 		SleepUtil.sleepSeconds(1);
 		Logger.info("Setting servo to min");
-		pz.setOutputValue(STEERING_GPIO, PiconZero.SERVO_CENTRE - SERVO_MAX_ANGLE_FROM_CENTRE);
+		pz.setOutputValue(STEERING_GPIO, SERVO_TRIM.getMinAngle());
 		SleepUtil.sleepSeconds(1);
 		Logger.info("Setting servo to max");
-		pz.setOutputValue(STEERING_GPIO, PiconZero.SERVO_CENTRE + SERVO_MAX_ANGLE_FROM_CENTRE);
+		pz.setOutputValue(STEERING_GPIO, SERVO_TRIM.getMaxAngle());
 		SleepUtil.sleepSeconds(1);
 		Logger.info("Setting servo to mid");
-		pz.setOutputValue(STEERING_GPIO, PiconZero.SERVO_CENTRE);
+		pz.setOutputValue(STEERING_GPIO, SERVO_TRIM.getMidAngle());
 	}
 
 	private void testLEDs() {
@@ -186,17 +191,5 @@ public class LegoCarApp implements AutoCloseable, JoystickEventListener {
 		Logger.info("LED off for channel {}", Integer.valueOf(channel));
 		pz.setOutputValue(channel, false);
 		SleepUtil.sleepSeconds(1);
-	}
-
-	@Override
-	public void close() {
-		if (joystick != null) {
-			joystick.close();
-		}
-		motor.close();
-		steering.close();
-		light1.close();
-		light2.close();
-		pz.close();
 	}
 }
